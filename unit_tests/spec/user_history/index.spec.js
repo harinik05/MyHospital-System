e6ba570b84e1a16fbf51d48685409a6590dae96b
harinik05 <hka747910@gmail.com>
2023-08-24 21:04:14 -0400

    updated the mock clients and new test case for checkfolderforpdf

--- unit_tests/spec/user_history/index.spec.js
+++ unit_tests/spec/user_history/index.spec.js
@@ -6,7 +6,7 @@ const {mockClient} = require("aws-sdk-client-mock");
 describe('Upload file to S3 Bucket', () => {
     let S3;
     beforeEach(() =>{
-        S3 = mockClient(AWS.S3);
+        S3 = mockClient(S3Client);
     });
     afterEach(() => {
         S3.restore();
@@ -17,12 +17,11 @@ describe('Upload file to S3 Bucket', () => {
 
     // Test case: Successful upload
     it('should upload a file to S3 successfully', async () => {
-        const S3 = mockClient(AWS.S3);
         // Mock the S3 upload function to resolve with a success message
         S3.mockResolvedValueOnce('upload', { Location: 'https://s3.amazonaws.com/bucket/test/file.txt' });
 
         try {
-            const result = await uploadFileToS3(bucketName, key, filePath, S3);
+            const result = await app.uploadFileToS3(bucketName, key, filePath, S3);
             expect(result).toBe('https://s3.amazonaws.com/bucket/test/file.txt');
         } catch (error) {
             // Handle any unexpected errors
@@ -37,7 +36,7 @@ describe('Upload file to S3 Bucket', () => {
         S3.mockRejectedValueOnce('upload', new Error('Upload failed'));
 
         try {
-            await uploadFileToS3(bucketName, key, filePath, S3);
+            await app.uploadFileToS3(bucketName, key, filePath, S3);
         } catch (error) {
             expect(error.message).toBe('Upload failed');
         } 
@@ -64,7 +63,7 @@ describe('readJSONFile', () => {
 
     // Test case 2: Handling an invalid JSON file
     it('should handle an invalid JSON file and return null', () => {
-        const result = readJSONFile(invalidFilePath);
+        const result = app.readJSONFile(invalidFilePath);
 
         // Make assertions about the result
         expect(result).toBeNull();
@@ -72,7 +71,59 @@ describe('readJSONFile', () => {
 
 });
 
+describe('checkFolderForPDF', () => {
+    let S3;
 
+    const bucketName = 'your-bucket-name';
+    const folderName = 'your-folder-name';
+
+    beforeEach(() => {
+        // Create a new mock S3 instance for each test case
+        S3 = mockClient(S3Client);
+    });
+
+    afterEach(() => {
+        // Restore the original behavior of the mock after each test case
+        S3.restore();
+    });
+
+    // Test case: PDF file exists in the folder
+    it('should return true if a PDF file exists in the folder', async () => {
+        // Mock the S3 listObjects method to return objects with a PDF file
+        S3.mockResolvedValueOnce('listObjects', {
+            Contents: [
+                { Key: 'your-folder-name/file1.pdf' },
+                { Key: 'your-folder-name/file2.txt' },
+            ],
+        });
+
+        const result = await app.checkFolderForPDF(bucketName, folderName, S3);
+        expect(result).toBe(true);
+    });
+
+    // Test case: No PDF file in the folder
+    it('should return false if no PDF file exists in the folder', async () => {
+        // Mock the S3 listObjects method to return objects without a PDF file
+        S3.mockResolvedValueOnce('listObjects', {
+            Contents: [
+                { Key: 'your-folder-name/file1.txt' },
+                { Key: 'your-folder-name/file2.jpg' },
+            ],
+        });
+
+        const result = await app.checkFolderForPDF(bucketName, folderName, S3);
+        expect(result).toBe(false);
+    });
+
+    // Test case: Error occurred during the operation
+    it('should handle an error during the S3 operation and return false', async () => {
+        // Mock the S3 listObjects method to throw an error
+        S3.mockRejectedValueOnce('listObjects', new Error('S3 operation failed'));
+
+        const result = await app.checkFolderForPDF(bucketName, folderName, S3);
+        expect(result).toBe(false);
+    });
+});
 
 
 
